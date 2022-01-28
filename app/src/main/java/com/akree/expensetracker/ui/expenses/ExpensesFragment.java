244cbec856f0ecc70565c137ac0ed5306ed0cfca
loveseo8 <maggaket@gmail.com>
2022-01-28 17:36:04 +0300

    +navigation

---
+++ app/src/main/java/com/akree/expensetracker/ui/expenses/ExpensesFragment.java
@@ -0,0 +1,45 @@
+package com.akree.expensetracker.ui.expenses;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.akree.expensetracker.databinding.FragmentExpensesBinding;
+
+public class ExpensesFragment extends Fragment {
+
+    private ExpensesViewModel expensesViewModel;
+    private FragmentExpensesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        expensesViewModel =
+                new ViewModelProvider(this).get(ExpensesViewModel.class);
+
+        binding = FragmentExpensesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textNotifications;
+        expensesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/akree/expensetracker/ui/expenses/ExpensesViewModel.java
@@ -0,0 +1,19 @@
+package com.akree.expensetracker.ui.expenses;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ExpensesViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ExpensesViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is expenses fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
