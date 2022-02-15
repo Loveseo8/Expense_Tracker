57452010a3041eec17998b3df57efbd4b0148c94
Mikhedov Konstantin <mihedovkos@gmail.com>
2022-02-15 10:38:08 +0300

    Add Profile data loading

--- app/src/main/java/com/akree/expensetracker/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/akree/expensetracker/ui/profile/ProfileFragment.java
@@ -8,18 +8,50 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.akree.expensetracker.R;
+import com.akree.expensetracker.User;
+import com.akree.expensetracker.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding binding = null;
+    private ProfileViewModel viewModel = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+
+        viewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+        viewModel.getUser().observe(getViewLifecycleOwner(), new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                updateDataFromUser(user);
+            }
+        });
+        updateDataFromUser(viewModel.getUser().getValue());
+
+        return binding.getRoot();
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private void updateDataFromUser(User user) {
+        if (user != null) {
+            binding.pfUserNameMsg.setText(user.getUsername());
+            binding.pfUserEmailMsg.setText(user.getEmail());
+
+            String [] nameParts = user.getUsername().split("\"\\\\s+\"");
+
+            StringBuilder nameInitials = new StringBuilder();
+            for (String part: nameParts ) { nameInitials.append(part.charAt(0)); }
+
+            binding.pfAvatarView.setAvatarInitials(nameInitials.toString().toUpperCase());
+        }
     }
 }
--- app/src/main/java/com/akree/expensetracker/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/akree/expensetracker/ui/profile/ProfileViewModel.java
@@ -4,16 +4,28 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.akree.expensetracker.User;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ProfileViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private final MutableLiveData<User> userData = new MutableLiveData<>(null);
 
     public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        FirebaseDatabase.getInstance()
+                .getReference("user/" + FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+                    @Override
+                    public void onSuccess(DataSnapshot dataSnapshot) {
+                        userData.setValue(dataSnapshot.getValue(User.class));
+                    }
+                });
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<User> getUser() {
+        return userData;
     }
 }
