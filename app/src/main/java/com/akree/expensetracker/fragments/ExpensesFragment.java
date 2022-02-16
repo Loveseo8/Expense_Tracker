e5be8a4110752f207addf043737983abe0c6c85d
Mikhedov Konstantin <mihedovkos@gmail.com>
2022-02-16 11:16:46 +0300

    Add view model and budhet loading

--- app/src/main/java/com/akree/expensetracker/fragments/ExpensesFragment.java
+++ app/src/main/java/com/akree/expensetracker/fragments/ExpensesFragment.java
@@ -8,18 +8,45 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.akree.expensetracker.R;
+import com.akree.expensetracker.databinding.FragmentExpensesBinding;
+import com.akree.expensetracker.models.ExpensesViewModel;
+import com.akree.expensetracker.serialization.Expense;
+
+import java.util.List;
+import java.util.Map;
 
 public class ExpensesFragment extends Fragment {
+    private FragmentExpensesBinding binding = null;
+    private ExpensesViewModel viewModel = null;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_expenses, container, false);
+        binding = FragmentExpensesBinding.inflate(inflater, container, false);
+
+        viewModel = new ViewModelProvider(this).get(ExpensesViewModel.class);
+        viewModel.getExpenses().observe(getViewLifecycleOwner(), new Observer<Map<String, Expense>>() {
+            @Override
+            public void onChanged(Map<String, Expense> stringExpenseMap) {
+                updateDataFromViewModel();
+            }
+        });
+        updateDataFromViewModel();
+
+        return binding.getRoot();
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private void updateDataFromViewModel() {
+        binding.efCurrentBalanceMsg.setText(viewModel.getBudget().getValue().toString());
     }
 }
