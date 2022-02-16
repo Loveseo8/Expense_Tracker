4fc3417d58fd2252fde8cd85fb6c231d27f2e658
Mikhedov Konstantin <mihedovkos@gmail.com>
2022-02-16 13:03:48 +0300

    Connect plots

--- app/src/main/java/com/akree/expensetracker/fragments/StatisticsFragment.java
+++ app/src/main/java/com/akree/expensetracker/fragments/StatisticsFragment.java
@@ -8,18 +8,56 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.akree.expensetracker.R;
+import com.akree.expensetracker.databinding.FragmentStatisticsBinding;
+import com.akree.expensetracker.models.ExpensesViewModel;
+import com.akree.expensetracker.serialization.Expense;
+
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
 
 public class StatisticsFragment extends Fragment {
+    private FragmentStatisticsBinding binding = null;
+    private ExpensesViewModel viewModel = null;
+
+    private int currentMonth = 0, currentYear = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+        binding = FragmentStatisticsBinding.inflate(inflater, container, false);
+
+        currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+        currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        updateTimePeriod();
+
+        viewModel = new ViewModelProvider(this).get(ExpensesViewModel.class);
+        viewModel.getExpenses().observe(getViewLifecycleOwner(), new Observer<Map<String, Expense>>() {
+            @Override
+            public void onChanged(Map<String, Expense> stringExpenseMap) {
+                updateStatisticFromViewModel();
+            }
+        });
+        updateStatisticFromViewModel();
+
+        return binding.getRoot();
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private void updateTimePeriod() {
+        binding.sfMonthMsg.setText(new DateFormatSymbols().getMonths()[currentMonth] + " " + Integer.toString(currentYear));
+    }
+
+    private void updateStatisticFromViewModel() {
+
     }
 }
