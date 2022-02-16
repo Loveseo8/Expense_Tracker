b0220dc37d5c188fe356803efbebd7c96101f710
Mikhedov Konstantin <mihedovkos@gmail.com>
2022-02-16 14:20:45 +0300

    Add outcomes plot

--- app/src/main/java/com/akree/expensetracker/fragments/StatisticsFragment.java
+++ app/src/main/java/com/akree/expensetracker/fragments/StatisticsFragment.java
@@ -1,7 +1,10 @@
 package com.akree.expensetracker.fragments;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,14 +19,19 @@ import com.akree.expensetracker.R;
 import com.akree.expensetracker.databinding.FragmentStatisticsBinding;
 import com.akree.expensetracker.models.ExpensesViewModel;
 import com.akree.expensetracker.serialization.Expense;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.android.gms.common.util.CollectionUtils;
 
 import java.text.DateFormatSymbols;
+import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -51,6 +59,8 @@ public class StatisticsFragment extends Fragment {
         });
         updateStatisticFromViewModel();
 
+        initChars();
+
         return binding.getRoot();
     }
 
@@ -58,6 +68,13 @@ public class StatisticsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void initChars() {
+        binding.sfOutcomesChart.getXAxis().setTextColor(getSecondaryColor());
+        binding.sfOutcomesChart.getAxisLeft().setTextColor(getSecondaryColor());
+        binding.sfOutcomesChart.getAxisRight().setTextColor(getSecondaryColor());
+        binding.sfOutcomesChart.getDescription().setEnabled(false);
     }
 
     private void updateTimePeriod() {
@@ -78,10 +95,45 @@ public class StatisticsFragment extends Fragment {
                             expense.getType().equals("Outcome");
                 }).collect(Collectors.toList());
 
+        int daysPerMonth = (new GregorianCalendar(currentYear, currentMonth, 1))
+                .getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        ArrayList<Double> days = new ArrayList<>(daysPerMonth);
+        for (int i = 0; i < daysPerMonth; i++) days.add(i, 0.0);
+
         double totalOutcomes = 0.0;
         for (Expense expense: perMonth) {
             totalOutcomes += expense.getAmount();
+
+            int expenseDay = Integer.parseInt(expense.getDate().split("\\.")[0]);
+            days.set(expenseDay, expense.getAmount() + days.get(expenseDay));
         }
         binding.sfTotalOutcomesMsg.setText(Double.valueOf(totalOutcomes).toString());
+
+        ArrayList<Entry> outcomes = new ArrayList<>();
+        for (int i = 0; i < daysPerMonth; i++) {
+            outcomes.add(i, new Entry(i + 1, days.get(i).floatValue()));
+        }
+
+        LineDataSet outcomesDataSet = new LineDataSet(outcomes, "Real outcomes");
+        outcomesDataSet.setMode(LineDataSet.Mode.LINEAR);
+        outcomesDataSet.setDrawCircles(false);
+        outcomesDataSet.setColor(getPrimaryColor());
+        outcomesDataSet.setDrawValues(false);
+
+        binding.sfOutcomesChart.setData(new LineData(outcomesDataSet));
+        binding.sfOutcomesChart.invalidate();
+    }
+
+    private int getPrimaryColor() {
+        TypedValue typedValue = new TypedValue();
+        getContext().getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+        return typedValue.data;
+    }
+
+    private int getSecondaryColor() {
+        TypedValue typedValue = new TypedValue();
+        getContext().getTheme().resolveAttribute(R.attr.colorSecondary, typedValue, true);
+        return typedValue.data;
     }
 }
